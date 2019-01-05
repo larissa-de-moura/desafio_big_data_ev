f2c71be76dce14d879415073523a9c584e3ea738
larissa-de-moura <chamorromoura@gmail.com>
2019-01-05 16:39:21 -0200

    Delete E-_piloto_sem_podio_maior_pontuacao.sql

--- E-_piloto_sem_podio_maior_pontuacao.sql
+++
@@ -1,9 +0,0 @@
-select hp.piloto_id, p.apelido, sum(hp.pontuacao) as pontos
-from historico_piloto as hp
-join piloto p on
-p.id = hp.piloto_id
-where hp.piloto_id NOT IN (select distinct piloto_id from historico_piloto where posicao <= 3)
-group by hp.piloto_id
-order by sum(hp.pontuacao) desc
-limit 1;
-
