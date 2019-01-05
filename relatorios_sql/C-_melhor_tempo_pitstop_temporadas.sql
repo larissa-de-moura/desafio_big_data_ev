2b1a0fc218ad8d40e599d4606b3e8ee9feaa55f4
larissa-de-moura <chamorromoura@gmail.com>
2019-01-05 16:15:34 -0200

    Add files via upload

---
+++ relatorios_sql/C-_melhor_tempo_pitstop_temporadas.sql
@@ -0,0 +1,12 @@
+select min(t.tempo) as tempo, r.piloto_id, p.apelido, r.equipe_id, e.nome as nome_equipe, c.temporada_ano
+    from tempo_por_parada t
+    left join resultado r
+    on t.piloto_id = r.piloto_id
+join piloto p
+on p.id = r.piloto_id
+join equipe e
+on e.id = r.equipe_id
+join corrida c on
+c.id = t.corrida_id
+where t.tempo is not null
+group by c.temporada_ano
